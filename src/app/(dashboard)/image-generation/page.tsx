a261b169742fb905e1576265b0d0fe791163c9c8
Balekage Gael <gbalekage21@gmail.com>
2025-05-30 14:25:04 +0200

    Image Genaration Ui

--- src/app/(dashboard)/image-generation/page.tsx
+++ src/app/(dashboard)/image-generation/page.tsx
@@ -1,8 +1,15 @@
+import Configurations from '@/components/image-generation/Configurations'
+import GeneratedItems from '@/components/image-generation/GeneratedItems'
 import React from 'react'
 
 const ImageGeneration = () => {
   return (
-    <div>ImageGeneration</div>
+    <section className='container mx-auto grid gap-4 grid-cols-3 overflow-hidden'>
+      <Configurations />
+      <div className='col-span-2 p-4 rounded-xl flex items-center justify-center h-fit'>
+        <GeneratedItems />
+      </div>
+    </section>
   )
 }
 
